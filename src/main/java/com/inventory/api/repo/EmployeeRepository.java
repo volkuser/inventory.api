0caac659b980387e430bd27a26870f7a6a8df18e
volkuser <demidov008h@gmail.com>
2023-06-02 00:03:25 +0300

    delete extra elements

--- src/main/java/com/inventory/api/repo/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.inventory.api.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.inventory.api.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
-
--- src/main/java/com/inventory/api/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.inventory.api.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.inventory.api.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
